e458520e5eaec34d1aebe03863dd34122fa78d35
Ahmet Tarık Özben <tarikozben@icloud.com>
2026-09-20 16:28:23 +0300

    Add Math Quiz Game

---
+++ MathQuizGame/MathQuizGame.java
@@ -0,0 +1,9 @@
+package MathQuizGame;
+
+public class MathQuizGame {
+    //Main method to start the quiz game
+    public static void main(String[] args) {
+        MathQuiz quiz = new MathQuiz();
+        quiz.startQuiz();
+    }
+}
